4817b8ef1c0677d55faf62a44788c2e08a613905
Kirthi Krishnamraju <kirthik@microsoft.com>
2012-01-03 16:14:54 -0800

    initial checkin

---
+++ RiaBat.Web/DomainService1.cs
@@ -0,0 +1,48 @@
+﻿
+namespace RiaBat.Web
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.ServiceModel.DomainServices.Hosting;
+    using System.ServiceModel.DomainServices.Server;
+
+
+    // TODO: Create methods containing your application logic.
+    [EnableClientAccess()]
+    public class DomainService1 : DomainService
+    {
+        List<ClientEntity> entities = new List<ClientEntity>();
+
+        public DomainService1()
+        {
+            entities.Add(new ClientEntity() { Id = 1, Name = "One" });
+            entities.Add(new ClientEntity() { Id = 2, Name = "Two" });
+        }
+
+        [Query]
+        public IEnumerable<ClientEntity> GetClientEntities()
+        {
+            return entities.AsEnumerable();
+        }
+
+        [Insert]
+        public void InsertClientEntity(ClientEntity ent)
+        {
+            entities.Add(ent);
+        }
+    }
+
+    public class ClientEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
+
+
---
+++ RiaBat/MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿namespace RiaBat
+{
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Navigation;
+    using RiaBat.LoginUI;
+    using RiaBat.Web;
+    using System;
+
+    /// <summary>
+    /// <see cref="UserControl"/> class providing the main UI for the application.
+    /// </summary>
+    public partial class MainPage : UserControl
+    {
+        DomainService1 ds1 = new DomainService1();
+        /// <summary>
+        /// Creates a new <see cref="MainPage"/> instance.
+        /// </summary>
+        public MainPage()
+        {
+            InitializeComponent();
+            domainDataSource1.LoadedData += new EventHandler<LoadedDataEventArgs>(domainDataSource1_LoadedData);
+
+            domainDataSource1.QueryName = "GetClientEntities";
+            domainDataSource1.DomainContext = ds1;
+
+        }
+
+        private void domainDataSource1_LoadedData(object sender, LoadedDataEventArgs e)
+        {
+
+            if (e.HasError)
+            {
+                System.Windows.MessageBox.Show(e.Error.ToString(), "Load Error", System.Windows.MessageBoxButton.OK);
+                e.MarkErrorAsHandled();
+            }
+        }
+
+        /// <summary>
+        /// After the Frame navigates, ensure the <see cref="HyperlinkButton"/> representing the current page is selected
+        /// </summary>
+        private void ContentFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+            foreach (UIElement child in LinksStackPanel.Children)
+            {
+                HyperlinkButton hb = child as HyperlinkButton;
+                if (hb != null && hb.NavigateUri != null)
+                {
+                    if (hb.NavigateUri.ToString().Equals(e.Uri.ToString()))
+                    {
+                        VisualStateManager.GoToState(hb, "ActiveLink", true);
+                    }
+                    else
+                    {
+                        VisualStateManager.GoToState(hb, "InactiveLink", true);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// If an error occurs during navigation, show an error window
+        /// </summary>
+        private void ContentFrame_NavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            e.Handled = true;
+            ErrorWindow.CreateNew(e.Exception);
+        }
+    }
+}
